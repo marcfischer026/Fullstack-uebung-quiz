0be5a586de5cb1231eb27cba2979bb1e82b7605e
marcfischer026 <marc.b.fischer@accenture.com>
2026-09-16 16:11:35 +0200

    erste rechenoperationen

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let text = "this is not a string"
 let booleanwert = true
 
 zahl = 6
-
 console.log(zahl);
 console.log(zahl-1);
 console.log(text);
-let newzahl = zahl
-console.log(newzahl+10);
+let newzahl = zahl+5
+console.log(newzahl+10/zahl);
 console.log(booleanwert)
 
 
