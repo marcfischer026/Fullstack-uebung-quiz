db0ee60283eb7577974909452c1a360b92fe4069
marcfischer026 <marc.b.fischer@accenture.com>
2026-09-16 16:55:16 +0200

    der tag ist vorbei

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let zahl = 5;
-let text = "this is not a string"
+let text = "this is not a string "
 let booleanwert = true
-
+let text2 = "this might be"
 zahl = 6
 console.log(zahl);
 console.log(zahl-1);
+console.log(zahl<=5)
 console.log(text);
 let newzahl = zahl+5
 console.log(newzahl+10/zahl);
 console.log(booleanwert)
+console.log(text+text2)
+console.log("a"<"b")
 
 
+function funktionsname(zahl,zahl2){
+    let result = zahl**zahl2
+    return result
+}
 
+console.log(funktionsname(42,5));
