f7cb06ef8c78edb93327b074222ac16e96d33c54
marcfischer026 <marc.b.fischer@accenture.com>
2026-09-16 15:44:56 +0200

    erstes hello world

---
+++ script.js
@@ -0,0 +1,15 @@
+let zahl = 5;
+let text = "this is not a string"
+let booleanwert = true
+
+zahl = 6
+
+console.log(zahl);
+console.log(zahl-1);
+console.log(text);
+let newzahl = zahl
+console.log(newzahl+10);
+console.log(booleanwert)
+
+
+
