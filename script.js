d469b136cb5e278777b5a3f26d03fa5fcd131575
marcfischer026 <marc.b.fischer@accenture.com>
2026-09-17 13:06:23 +0200

    Java script if & switch case

--- script.js
+++ script.js
@@ -15,8 +15,72 @@ console.log("a"<"b")
 
 
 function funktionsname(zahl,zahl2){
-    let result = zahl**zahl2
+    let result = zahl+zahl2
     return result
 }
+let zahl3 = 0 ;
+zahl3 = funktionsname(5,5);
+console.log(zahl3) ;
 
-console.log(funktionsname(42,5));
+
+if (zahl3>=15){
+zahl3=15
+}
+else {
+    zahl3-=20
+}
+console.log(zahl3)
+
+let gleicheZahl = 6; //erste Zahl
+let gleicheZahl1 = 5; //zweite Zahl
+
+if (gleicheZahl == gleicheZahl1){
+    console.log("gleich")   
+}
+else{
+    if (gleicheZahl>gleicheZahl1){
+        console.log(gleicheZahl+" (erste Zahl größer)")
+    }
+    else{
+        console.log(gleicheZahl1+" (zweite Zahl größer)")
+    }
+
+}
+
+
+function evenOrUneven (zahl) {
+
+    if (zahl % 2 ==0 ) {
+        console.log("even")
+    }
+    else {
+        console.log("uneven")
+    }
+   
+}
+evenOrUneven(4)
+let switZahl = 1;
+let canBreak;
+switch (switZahl) {
+    case 1: 
+    case 2: canBreak = false
+    break
+    case 3: canBreak = true
+    break
+}
+console.log("can zombies break doors "+ canBreak)
+
+
+let dificulty;
+let canBreaktenary;
+switch (switZahl){
+    case 1:dificulty = "easy"
+    break
+    case 2:dificulty = "medium"
+    break
+    case 3:dificulty = "hard" 
+    break
+
+}
+canBreaktenary = (dificulty =="hard") ? "zombie can break the door" : "zombie cant break the door";
+console.log(canBreaktenary)
